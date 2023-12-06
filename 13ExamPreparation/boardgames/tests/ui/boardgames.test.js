e7efac0638d705e21bab67735e6aae2825987fcf
Miroslav Gechev <vindaloo89@gmail.com>
2023-12-06 21:50:39 +0200

    Exam Preparation Tasks

---
+++ 13ExamPreparation/boardgames/tests/ui/boardgames.test.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test('Check boardgames page', async ({ page }) => {
+  await page.goto('https://miroslavgechev.onrender.com/boardgames');
+  const list = await page.$('ul');
+  expect(list).toBeTruthy();
+});
